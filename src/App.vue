42d8b00834828fede1b1e0549405e3df85a8747d
doniyorrr <elmurodovdoniyor589@gmail.com>
2022-10-22 11:24:34 +0500

    ui button created

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,18 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+  <Navbar title="Project"/>
+  <h1>hello world</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <!-- <RouterView /> -->
 </template>
 
 <style scoped>
+/* 
 header {
   line-height: 1.5;
   max-height: 100vh;
@@ -86,6 +72,6 @@ nav a:first-of-type {
 
     padding: 1rem 0;
     margin-top: 1rem;
-  }
-}
+  } */
+/* } */
 </style>
